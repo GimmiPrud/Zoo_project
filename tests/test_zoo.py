3618ffb1b3fa9e47d6eb936a81c5d30392fa1b60
GimmiPrud <prudenzig@gmail.com>
2024-06-05 13:45:25 +0200

    ...

--- tests/test_zoo.py
+++ tests/test_zoo.py
@@ -1,4 +1,4 @@
-import unittest 
+import unittest
 from unittest import TestCase
 from src.zoo import Zoo, ZooKeeper, Animal, Fence  # src.zoo = cartella.file e si importano le funzioni di zoo
 
@@ -14,6 +14,45 @@ class Testzoo(TestCase):
     # Test sul progetto zoo.py:
 
     def test_animal_dimension(self):
+
         zookeeper1: ZooKeeper = ZooKeeper(name="Alessio",surname="Carlini",id=379)
-        fence1: Fence = Fence(area=300, temperature=21.0,habitat="Savanna")
+        fence1: Fence = Fence(area=100, temperature=21.0,habitat="Savanna")
+        animal1: Animal = Animal(name="Bill",species="Felide", age=2, height=10.0,width=20.0, preferred_habitat="Savanna")
+        zookeeper1.add_animal(animal1,fence1)
+        result: int = len(fence1.animals)
+        message: str = f"Error: the function add_animal should not add self.animal1 into self.fence1"
+
+        self.assertEqual(result, 0, message)
+
+
+    def test_animal_habitat(self):
+
+        zookeeper1: ZooKeeper = ZooKeeper(name="Alessio",surname="Carlini",id=379)
+        fence1: Fence = Fence(area=300, temperature=21.0,habitat="jungle")
         animal1: Animal = Animal(name="Bill",species="Felide", age=2, height=10.0,width=1.50, preferred_habitat="Savanna")
+        zookeeper1.add_animal(animal1,fence1)
+        result: int = len(fence1.animals)
+        message: str = f"Error: the function add_animal should not add self.animal1 into self.fence1"
+
+        self.assertEqual(result, 0, message)
+
+
+    def test_remove_animal(self):
+        
+        zookeeper1: ZooKeeper = ZooKeeper(name="Alessio",surname="Carlini",id=379)
+        fence1: Fence = Fence(area=300, temperature=21.0,habitat="jungle")
+        animal1: Animal = Animal(name="Bill",species="Felide", age=2, height=10.0,width=1.50, preferred_habitat="Savanna")
+        zookeeper1.remove_animal(animal1,fence1)
+        result: int = len(fence1.animals)
+        message: str = f"Error: the function remove_animal should not remove self.animal1 into self.fence1"
+
+        self.assertEqual(result, 0, message)
+    
+    
+
+
+
+
+if __name__=="__main__":
+    unittest.main()
+
